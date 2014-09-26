7642c9c847018257398c365f0cf0da964e27fc58
chrisjohnson89 <afckazoo@gmail.com>
2014-09-25 23:12:27 -0700

    local variable

--- intro-rails-ruby/flashcards.rb
+++ intro-rails-ruby/flashcards.rb
@@ -1,26 +1,26 @@
-# card = Card.new
+#card = Card.new
 class Card
-	def initialize
-		puts "hello"
+	def initialize(front, back)
+		puts "#{front} => #{back}"
 	end
 end
 
-card1 = Card.new
-card2 =
+card1 = Card.new("cat", "neko")
+card2 = Card.new("dog", "inu")
 
-deck = [card1, card2]
+#deck = [card1, card2]
 
-deck.each do |card|
-	front = card[:front]
-	back = card[:back]
+#deck.each do |card|
+	#front = card[:front]
+	#back = card[:back]
 
-	print "#{front} > "
-	guess = gets.chomp
+	#print "#{front} > "
+	#guess = gets.chomp
 
-	if guess == back
-		puts "Correct"
-	else
-		puts "Incorrect. The answer was #{back}"
-	end
+	#if guess == back
+		#puts "Correct"
+	#else
+		#puts "Incorrect. The answer was #{back}"
+	#end
 
-end
+#end
